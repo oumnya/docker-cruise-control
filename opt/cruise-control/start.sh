5e27ef60dd1011bf8fc9f9a3b999d5838dfce8a2
oumnya <oumnya.benhassou@gmail.com>
2023-08-16 02:01:09 +0000

    allow override params

--- opt/cruise-control/start.sh
+++ opt/cruise-control/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+
 cd /opt/cruise-control
 
 # Set heap memory settings for container environments
@@ -10,4 +11,19 @@ fi
 if [ -z "${KAFKA_OPTS}" ]; then
   export KAFKA_OPTS="-Dsun.net.inetaddr.ttl=60"
 fi
-/bin/bash ${DEBUG:+-x} /opt/cruise-control/kafka-cruise-control-start.sh /opt/cruise-control/config/cruisecontrol.properties 8090
+
+# Default configuration file path
+CONFIG_FILE="/opt/cruise-control/config/cruisecontrol.properties"
+
+# If a path argument is provided, use that as the configuration file path
+if [ "$#" -gt 0 ]; then
+    CONFIG_FILE="$1"
+fi
+
+# If CRUISE_CONTROL_PROPERTIES_CONTENT is set, overwrite the default file
+if [ ! -z "$CRUISE_CONTROL_PROPERTIES_CONTENT" ]; then
+    echo "$CRUISE_CONTROL_PROPERTIES_CONTENT" > $CONFIG_FILE
+fi
+
+# Execute the Kafka Cruise Control process
+exec "/opt/cruise-control/kafka-cruise-control-start.sh" $CONFIG_FILE 8090
